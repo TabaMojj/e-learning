42c3955ac08727cde8e0ba7579772e0cd33302b4
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-03 22:31:12 +0330

    feat: add cache to views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.db.models import Count
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import DetailView
@@ -146,10 +147,23 @@ class CourseListView(TemplateResponseMixin, View):
 
     def get(self, request, *args, **kwargs):
         subject = kwargs.get('subject')
-        subjects = Subject.objects.annotate(total_courses=Count('courses'))
-        courses = Course.objects.annotate(total_modules=Count('modules'))
+        subjects = cache.get('all_subjects')
+        if not subjects:
+            subjects = Subject.objects.annotate(total_courses=Count('courses'))
+            cache.set('all_subjects', subjects)
+        all_courses = Course.objects.annotate(total_modules=Count('modules'))
         if subject:
             subject = get_object_or_404(Subject, slug=subject)
+            key = f'subject_{subject.id}_courses'
+            courses = cache.get(key)
+            if not courses:
+                courses = all_courses.filter(subject=subject)
+                cache.set(key, courses)
+        else:
+            courses = cache.get('all_courses')
+            if not courses:
+                courses = all_courses
+                cache.set('all_courses', courses)
             courses = courses.filter(subject=subject)
         return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
 
