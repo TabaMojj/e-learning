c97a01a3fbcc6177d699b4198fbd38e569eb5159
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-03 18:36:29 +0330

    feat: add students application config

--- e_learning/settings.py
+++ e_learning/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'students.apps.StudentsConfig',
     'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
