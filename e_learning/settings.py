3d4b8af475126f7d04e57d1a6c892dc797bf5da1
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-04 19:34:28 +0330

    feat: add course and subject rest apis

--- e_learning/settings.py
+++ e_learning/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -137,4 +138,10 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': 'redis://127.0.0.1:6379',
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
